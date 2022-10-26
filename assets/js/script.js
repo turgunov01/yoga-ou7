13f9b2555abac29689939cb3ac115415795431fa
Ваше Имя <you@example.com>
2022-10-26 13:29:05 +0500

    reuse last swiper

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,26 +5,51 @@ const parallaxInstance = new Parallax(scene, {
 })
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 'auto',
+    // slidesPerView: 'auto',
+    // centeredSlides: true,
+    // centerInsufficientSlides: true,
+    // spaceBetween: 30,
+    // speed: 1000,
+    // loop: true,
+    // slideToClickedSlide: true,
+    // autoplay: {
+    //     delay: 3000,
+    //     duration: 3000,
+    //     trasition: 3000,
+    //     speed: 3000,
+    // },
+    // breakpoints: {
+    //     0: {
+    //         spaceBetween: 10,
+    //     },
+    //     320: {
+    //         spaceBetween: 30,
+    //     }
+    // }
+
+    initialSlides: 2,
     centeredSlides: true,
-    centerInsufficientSlides: true,
-    spaceBetween: 30,
-    speed: 1000,
     loop: true,
-    slideToClickedSlide: true,
+    slidesPerView: 'auto',
+    spaceBetween: 10,
+    speed: 1000,
+    duration: 2000,
+    transition: 500,
     autoplay: {
         delay: 3000,
-        duration: 3000,
-        trasition: 3000,
-        speed: 3000,
     },
+
     breakpoints: {
         0: {
-            spaceBetween: 10,
+            spaceBetween: -5,
         },
-        320: {
-            spaceBetween: 30,
+
+        375: {
+            spaceBetween: -20,
+        },
+
+        990: {
+            spaceBetween: -20,
         }
     }
 });
-
